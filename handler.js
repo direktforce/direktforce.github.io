1e6d0ea9aba1346bc888ee2cd4eaa917bdd7f745
Sam Zagrobelny <strangesast@gmail.com>
2020-05-05 11:08:15 -0400

    progress. tweaks

--- handler.js
+++ handler.js
@@ -3,63 +3,138 @@
 const aws = require('aws-sdk');
 const ses = new aws.SES();
 
-const toEmail = "sam.zagrobelny@gmail.com";
-const fromEmail = "strangesast@gmail.com";
+const ToAddresses = ['sam.zagrobelny@gmail.com']; // kwk@direktforce.com
+const CcAddresses = [`7165122455@msg.fi.google.com`, 'prizerebel_junk@live.com']; // sam.zagrobelny@gmail.com
+const SourceAddress = 'strangesast@gmail.com'; // eventually will be contact.direktforce.com
 
-
-const domainName = '*';
-const headers = {
+const domainName = '*'; // should update to direktforce.com / direktforce.github.io
+const HEADERS = {
   'Access-Control-Allow-Origin': domainName,
   'Access-Control-Allow-Headers': 'x-requested-with',
   'Access-Control-Allow-Credentials': true
 };
 
+const HTMLHead = `
+  <title>A Message From direktforce.com</title>
+  <style>
+    body {
+      margin: 0;
+    }
+    .details {
+      display: grid;
+      grid-template-columns: auto auto;
+      grid-gap: 8px;
+    }
+    .detail p {
+      grid-column: 1 / 3;
+    }
+    .detail > *:empty:after {
+      content: "(blank)";
+    }
+  </style>
+`;
+
+const DOMAIN = {
+  url: 'https://direktforce.com',
+  name: 'direktforce.com',
+};
+
+function formatContactEmail(body) {
+  const vals = ['name', 'email', 'company', 'message']
+    .map(key => key in body ? body[key] : '(blank)');
+
+  const TextData = [
+    `A message redirected from direktforce.com:`,
+    `Name: ${vals[0]}`, `Email: ${vals[1]}`,
+    `Company: ${vals[2]}`, `Message:`, `${vals[3]}`,
+  ].join('\n');
+
+  const HTMLData = `
+  <html><body>
+    <h1>New message redirected from <a href="${DOMAIN.url}">${DOMAIN.name}</a></h1>
+    <div style="display: grid; grid-template-columns: auto auto;">
+      <span>Name:</span><span>${body.name}</span>
+      <span>Email:</span><span>${body.email}</span>
+      <span>Company:</span><span>${body.company}</span>
+      <span>Message:</span>
+      <p style="grid-column: 1 / 3;">${body.message}</p>
+    </div>
+  </body></html>
+  `;
+  const SubjectData = `A message from ${body.name} (${body.email})`;
+
+  return {
+    Body: {
+      Text: { Data: TextData },
+      Html: { Data: HTMLData },
+    },
+    Subject: { Data: SubjectData },
+  };
+}
+
+function formatApplicationEmail(body) {
+  const vals = ['name', 'email', 'message']
+    .map(key => key in body ? body[key] : '(blank)');
+
+  const TextData = [
+    `A new application redirected from direktforce.com:`,
+    `Name: ${vals[0]}`, `Email: ${vals[1]}`, `Message:`, `${vals[2]}`,
+  ].join('\n');
+
+  const HTMLData = `
+  <html><body>
+    <h1>New application redirected from <a href="${DOMAIN.url}">${DOMAIN.name}</a></h1>
+    <div style="display: grid; grid-template-columns: auto auto;">
+      <span>Name:</span><span>${body.name}</span>
+      <span>Email:</span><span>${body.email}</span>
+      <span>Message:</span>
+      <p style="grid-column: 1 / 3;">${body.message}</p>
+    </div>
+  </body></html>
+  `;
+  const SubjectData = `A new application from ${body.name} (${body.email})`;
+
+  return {
+    Body: {
+      Text: { Data: TextData },
+      Html: { Data: HTMLData },
+    },
+    Subject: { Data: SubjectData },
+  };
+}
 
 module.exports.staticSiteMailer = async (event, context) => {
-  let name, email, company, message;
+  let body;
   try {
-    ({name, email, company, message} = JSON.parse(event.body));
+    body = JSON.parse(event.body) || {};
+    if (!body.type || (body.type !== "application" && body.type != "contact")) throw new Error("invalid message type");
   } catch (err) {
     return {
       statusCode: 400,
       body: JSON.stringify(err.message),
-      headers,
+      headers: HEADERS,
     };
   }
-
-  const Data = [
-    `A message redirected from direktforce.com:`,
-    `Name: ${name || '(blank)'}`,
-    `Email: ${email || '(blank)'}`
-    `Company: ${company || '(blank)'}`
-    `Message:`,
-    `${message || '(blank)'}`,
-  ].join('\n');
-
-  const emailParams = {
-    Destination: {
-      ToAddresses: [toEmail],
-    },
-    Message: {
-      Body: { Text: { Data } },
-      Subject: { Data: `A message from ${name} (${email})` },
-    },
-    Source: fromEmail,
-  };
-
   try {
-    const data = await ses.sendEmail(emailParams).promise();
+    const result = await ses.sendEmail({
+      Message: body.type == 'contact' ? formatContactEmail(body): formatApplicationEmail(body),
+      Destination: {
+        ToAddresses,
+        CcAddresses,
+      },
+      Source: SourceAddress,
+    }).promise();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({message: 'Message sent successfully', result, body}),
+      headers: HEADERS,
+    };
   } catch (err) {
     return {
       statusCode: 500,
       body: JSON.stringify(err.message),
-      headers,
+      headers: HEADERS,
     };
   }
-
-  return {
-    statusCode: 200,
-    body: JSON.stringify({message: 'Message sent successfully', params: {name, email, company, message}}),
-    headers,
-  };
 };
--- src/index.js
+++ src/index.js
@@ -2,3 +2,139 @@ require('./header.scss');
 require('./footer.scss');
 require('./style.scss');
 require('./index.scss');
+
+const ENDPOINT_URL = 'https://wy2rngkmq2.execute-api.us-east-1.amazonaws.com/dev/static-site-mailer';
+const LOCALSTORAGE_KEY_ATTR = 'data-localstorage-key';
+
+function getSavedState(key) {
+  try {
+    const state = JSON.parse(localStorage.getItem(key)) || {};
+    if (state.submittedOn != null) {
+      state.submittedOn = new Date(state.submittedOn);
+    }
+    return state;
+  } catch (err) {
+    localStorage.removeItem(key);
+    return {};
+  }
+}
+
+function setSavedState(key, value) {
+  localStorage.setItem(key, JSON.stringify(value));
+}
+
+function setFormState(formEl, state) {
+  const { submitted, submittedOn, value } = state;
+
+  for (const el of formEl.querySelectorAll('input,textarea')) {
+    const key = el.getAttribute('name');
+    el.value = value[key] || '';
+  }
+
+  if (submitted) {
+    const submitButtonEl = formEl.querySelector('button[type="submit"]');
+    formEl.querySelector('.status-message').textContent = `Message sent on ${submittedOn.toLocaleDateString('en-us')} at ${submittedOn.toLocaleTimeString('en-us')}.`;
+    submitButtonEl.textContent = 'Message Sent!';
+    submitButtonEl.classList.add('success');
+
+    formEl.querySelector('fieldset').setAttribute('disabled', '');
+  }
+}
+
+function getFormState(formEl) {
+  const fd = new FormData(formEl);
+  const obj = {};
+  for (const [key, value] of fd.entries()) {
+    obj[key] = value;
+  }
+  return {value: obj};
+}
+
+function setFormLoading(formEl) {
+  const submitButtonEl = formEl.querySelector('button[type="submit"]');
+  submitButtonEl.textContent = 'Sending...';
+  formEl.querySelector('fieldset').setAttribute('disabled', '');
+}
+
+function setFormErrored(formEl) {
+  const submitButtonEl = formEl.querySelector('button[type="submit"]');
+  submitButtonEl.textContent = 'Send';
+  submitButtonEl.classList.remove('success');
+  formEl.querySelector('fieldset').removeAttribute('disabled');
+  formEl.querySelector('.status-message').textContent = `Message failed to send. Please try again or send us an email.`;
+}
+
+function setFormSuccess(formEl) {
+  const submitButtonEl = formEl.querySelector('button[type="submit"]');
+  submitButtonEl.textContent = 'Message Sent!';
+  submitButtonEl.classList.add('success');
+}
+
+function setFormReset(formEl) {
+  formEl.querySelector('fieldset').removeAttribute('disabled');
+  for (const el of formEl.querySelectorAll('input,textarea')) {
+    el.value = '';
+  }
+}
+
+async function sendMessage(value) {
+  const options = {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(value),
+  };
+  const res = await fetch(ENDPOINT_URL, options);
+  if (res.status >= 400 || res.status < 200) {
+    throw new Error(`request failed with code ${res.status}`);
+  }
+  return res.json();
+}
+
+function onSubmit(event) {
+  const formEl = event.srcElement;
+  (async () => {
+    const {value} = getFormState(formEl);
+    const localStorageKey = formEl.getAttribute(LOCALSTORAGE_KEY_ATTR);
+    const submittedOn = new Date();
+    setSavedState({value});
+    setFormLoading(formEl);
+    const result = await sendMessage(value);
+    try {
+      const state = {submitted: true, submittedOn, value};
+      setSavedState(localStorageKey, state);
+      setFormSuccess(formEl);
+    } catch (err) {
+      setFormErrored(formEl);
+    }
+  })();
+  event.preventDefault();
+  event.stopPropagation();
+  return false;
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  for (const formEl of document.querySelectorAll('form')) {
+    const localStorageKey = formEl.getAttribute(LOCALSTORAGE_KEY_ATTR);
+    if (localStorageKey == null) {
+      continue;
+    }
+    try {
+      const state = getSavedState(localStorageKey);
+      setFormState(formEl, state);
+    } catch (err) {
+      console.log(err);
+      localStorage.removeItem(localStorageKey);
+    }
+    formEl.oninput = ((key) => (e) => {
+      const el = e.target;
+      const state = getSavedState(key);
+      const value = state.value || {};
+      value[el.getAttribute('name')] = el.value;
+      setSavedState(key, Object.assign(state, {value}));
+    })(localStorageKey);
+    formEl.onsubmit = onSubmit;
+  }
+});
